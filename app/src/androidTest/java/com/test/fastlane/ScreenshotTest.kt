34989052d3ae8475fcbbe4c26ce0f04322013d3a
Bogdan Pechounov <bogdan.pechounov@outlook.com>
2023-12-24 12:47:48 -0500

    init

--- app/src/androidTest/java/com/test/fastlane/ScreenshotTest.kt
+++ app/src/androidTest/java/com/test/fastlane/ScreenshotTest.kt
@@ -28,10 +28,10 @@ class ScreenshotTest {
 
     @Before
     fun init() {
-//        Screengrab
-//            .setDefaultScreenshotStrategy(
-//                UiAutomatorScreenshotStrategy()
-//            )
+        Screengrab
+            .setDefaultScreenshotStrategy(
+                UiAutomatorScreenshotStrategy()
+            )
     }
 
     @Test
