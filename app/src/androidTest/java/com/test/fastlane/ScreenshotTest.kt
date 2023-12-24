80d30ef34c81305fd6eb1eabf8bf90b2b9abdc47
Bogdan Pechounov <bogdan.pechounov@outlook.com>
2023-12-24 12:36:33 -0500

    init

---
+++ app/src/androidTest/java/com/test/fastlane/ScreenshotTest.kt
@@ -0,0 +1,49 @@
+package com.test.fastlane
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.test.fastlane.ui.MainScreen
+import com.test.fastlane.ui.theme.FastlaneTestTheme
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import tools.fastlane.screengrab.Screengrab
+import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ScreenshotTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Before
+    fun init() {
+//        Screengrab
+//            .setDefaultScreenshotStrategy(
+//                UiAutomatorScreenshotStrategy()
+//            )
+    }
+
+    @Test
+    fun useAppContext() {
+        Screengrab.screenshot("before");
+
+        composeTestRule.setContent {
+            FastlaneTestTheme {
+                MainScreen()
+            }
+        }
+
+        Screengrab.screenshot("after");
+    }
+}
---
+++ app/src/main/java/com/test/fastlane/ui/MainScreen.kt
@@ -0,0 +1,16 @@
+package com.test.fastlane.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MainScreen() {
+    Text(text = "Main screen")
+}
+
+@Preview
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
+}
